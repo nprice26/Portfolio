c900f5319e1d1ccc6fa66b5640e61e1ab007277e
nprice26 <77947743+nprice26@users.noreply.github.com>
2025-09-09 14:44:14 -0400

    Delete SQL_Project_Sales/Sales by Region.sql

--- SQL_Project_Sales/Sales by Region.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  Region,
-  ROUND(SUM(Sales),2) AS Regional_Sales,
-  ROUND(SUM(Profit),2) AS Regional_Profit
-FROM `superstore-data-471614.superstore.data`
-GROUP BY Region
-ORDER BY Regional_Sales DESC
